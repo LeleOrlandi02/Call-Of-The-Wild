ae0b0ec6d9ffdfaed82ed62e48cab181f27a6e8d
EdoConfo <edo.conforti@stud.uniroma3.it>
2025-08-19 23:29:48 +0200

    Aggiunto combattimento con nemici (è anche stranamente difficile), aggiunti gizmos vari per attackPoints, i nemici ora hanno la vita

--- Assets/Script/Warrior/Player_Attack.cs
+++ Assets/Script/Warrior/Player_Attack.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player_Attack : MonoBehaviour
 {
+    public Transform attackPoint;
+    public float weaponRange = 1;
+    public LayerMask enemyLayer;
+    public int damage = 1;
+
     public Animator anim;
 
     public float cooldown = 2;
@@ -26,8 +31,29 @@ public class Player_Attack : MonoBehaviour
         }
     }
 
+    public void DealDamage()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+
+        foreach (Collider2D enemy in enemies)
+        {
+            if (enemy.isTrigger) continue;
+
+            if (enemies.Length > 0)
+            {
+                enemies[0].GetComponent<Enemy_Health>().ChangeHealth(-damage);
+            }
+        }
+    }
+
     public void FinishAttaccking()
     {
         anim.SetBool("isAttacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, weaponRange);
     }
 }
