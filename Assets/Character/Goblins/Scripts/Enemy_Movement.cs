47a68e2f023123473b8196a86741d0ecc9d803bf
EdoConfo <edo.conforti@stud.uniroma3.it>
2025-08-19 18:02:46 +0200

    Aggiunto l'inseguimento del player per il nemico, il facing verso il player e le animazioni per i goblin
    Moved Goblin and Warrior scripts, animations, and related assets into more structured subfolders under Character and Tiles. Added new Goblin running animation and animator controller. Updated Idle animation to use sprite curves and loop. Updated scene and asset references to match new structure.

---
+++ Assets/Character/Goblins/Scripts/Enemy_Movement.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Movement : MonoBehaviour
+{
+    public float speed;
+    private int facingDirection = -1;
+    private EnemyState enemyState;
+
+    private Rigidbody2D rb;
+    private Transform player;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyState == EnemyState.Chasing)
+        {
+            if(player.position.x > transform.position.x && facingDirection == -1 ||
+               player.position.x < transform.position.x && facingDirection == 1)
+            {
+                Flip();
+            }
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.velocity = direction * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if(player == null)
+            {
+                player = collision.transform;
+            }
+            ChangeState(EnemyState.Chasing);
+        }
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.velocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+        }
+    }
+
+    void ChangeState(EnemyState newState)
+    {
+        if(enemyState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", false);
+        }
+        else if(enemyState == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", false);
+        }
+        enemyState = newState;
+
+        if (enemyState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", true);
+        }
+        else if (enemyState == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", true);
+        }
+    }
+}
+
+public enum EnemyState
+{
+    Idle,
+    Chasing,
+}
