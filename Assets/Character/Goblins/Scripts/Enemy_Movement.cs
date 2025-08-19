a391e92f9a67bd56329c47a17178152477edb390
EdoConfo <edo.conforti@stud.uniroma3.it>
2025-08-19 18:57:20 +0200

    Aggiunte animazioni per l'attacco dei goblin
    Introduced AttackDown, AttackUp, and AttackLeftRight animation clips for goblins and updated the animator controller to support attack states. Enhanced Enemy_Combat and Enemy_Movement scripts to implement attack detection, cooldowns, and player targeting using OverlapCircle checks. Updated the scene to wire up new attack and detection points, adjusted UI element positions, and set up relevant layer masks for player detection.

--- Assets/Character/Goblins/Scripts/Enemy_Movement.cs
+++ Assets/Character/Goblins/Scripts/Enemy_Movement.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Enemy_Movement : MonoBehaviour
 {
     public float speed;
+    public float attackRange = 2;
+    public float attackCooldown = 2;
+    public float playerDetectRange = 5;
+    public Transform detectionPoint;
+    public LayerMask playerLayer;
+
+    private float attackCooldownTimer;
     private int facingDirection = -1;
     private EnemyState enemyState;
 
@@ -23,27 +30,58 @@ public class Enemy_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckForPlayer();
+
+        if(attackCooldownTimer > 0)
+        {
+            attackCooldownTimer -= Time.deltaTime;
+        }
+
         if (enemyState == EnemyState.Chasing)
         {
-            if(player.position.x > transform.position.x && facingDirection == -1 ||
-               player.position.x < transform.position.x && facingDirection == 1)
-            {
-                Flip();
-            }
-            Vector2 direction = (player.position - transform.position).normalized;
-            rb.velocity = direction * speed;
+            Chase();
+        }
+        else if (enemyState == EnemyState.Attacking)
+        {
+            rb.velocity = Vector2.zero;
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void Chase()
     {
-        if (collision.gameObject.tag == "Player")
+         if (player.position.x > transform.position.x && facingDirection == -1 ||
+               player.position.x < transform.position.x && facingDirection == 1)
         {
-            if(player == null)
+            Flip();
+        }
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.velocity = direction * speed;
+    }
+
+    private void CheckForPlayer()
+    {
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectRange, playerLayer);
+
+        if (hits.Length > 0)
+        {
+            player = hits[0].transform;
+
+            if (Vector2.Distance(transform.position, player.position) <= attackRange && attackCooldownTimer <= 0)
             {
-                player = collision.transform;
+                attackCooldownTimer = attackCooldown;
+                ChangeState(EnemyState.Attacking);
             }
-            ChangeState(EnemyState.Chasing);
+
+            else if (Vector2.Distance(transform.position, player.position) > attackRange)
+            {
+                ChangeState(EnemyState.Chasing);
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
         }
     }
 
@@ -51,15 +89,6 @@ public class Enemy_Movement : MonoBehaviour
     {
         facingDirection *= -1;
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            rb.velocity = Vector2.zero;
-            ChangeState(EnemyState.Idle);
-        }
     }
 
     void ChangeState(EnemyState newState)
@@ -72,6 +101,10 @@ public class Enemy_Movement : MonoBehaviour
         {
             anim.SetBool("isChasing", false);
         }
+        else if (enemyState == EnemyState.Attacking)
+        {
+            anim.SetBool("isAttacking", false);
+        }
         enemyState = newState;
 
         if (enemyState == EnemyState.Idle)
@@ -82,6 +115,16 @@ public class Enemy_Movement : MonoBehaviour
         {
             anim.SetBool("isChasing", true);
         }
+        else if (enemyState == EnemyState.Attacking)
+        {
+            anim.SetBool("isAttacking", true);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(detectionPoint.position, playerDetectRange);
     }
 }
 
@@ -89,4 +132,5 @@ public enum EnemyState
 {
     Idle,
     Chasing,
+    Attacking,
 }
