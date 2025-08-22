20129f43fb6eb2911f41d75275e88fc1a359464f
EdoConfo <edo.conforti@stud.uniroma3.it>
2025-08-22 15:28:01 +0200

    Aggiunti sistema di transizione scena persistenti e animazioni e prefab dell'oggetto cuore curativo
    Introduced new animations and animator controllers for item pickups, specifically the Healing Heart, including 'Heart_Pickup' and 'Healing Heart' controller. Added supporting prefabs for items, loot, and persistent objects, and updated scene files to integrate these assets. Also added fade in/out animations and scripts for scene transitions.

---
+++ Assets/Script/Items/Loot.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour
+{
+    public ItemSO itemSO;
+    public SpriteRenderer sr;
+    public Animator anim;
+
+    public int quantity;
+
+    private void OnValidate()
+    {
+        if(itemSO == null)
+        {
+            return;
+        }
+        sr.sprite = itemSO.icon;
+        this.name = itemSO.itemName;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            anim.Play("Heart_Pickup");
+            Destroy(gameObject, .5f);
+            if(collision.gameObject.GetComponent<Player_Health>().currentHealth < collision.gameObject.GetComponent<Player_Health>().maxHealt)
+            {
+                collision.gameObject.GetComponent<Player_Health>().ChangeHealth(quantity);
+            }
+        }
+    }
+}
